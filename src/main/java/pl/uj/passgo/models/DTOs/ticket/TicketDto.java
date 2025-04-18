ae980247869636a557e7927e312b89e514d17bfa
Pajtus <piotrus.umanski@gmail.com>
2025-04-18 15:55:08 +0200

    feat(ticketMapper): implemented ticketMapper with TicketDto

---
+++ src/main/java/pl/uj/passgo/models/DTOs/ticket/TicketDto.java
@@ -0,0 +1,19 @@
+package pl.uj.passgo.models.DTOs.ticket;
+
+import pl.uj.passgo.models.DTOs.client.ClientDto;
+import pl.uj.passgo.models.DTOs.event.EventDto;
+
+import java.math.BigDecimal;
+
+
+public record TicketDto(
+	Long id,
+	BigDecimal price,
+	EventDto event,
+	Long sectorId,
+	Long rowId,
+	Long seatId,
+	boolean standingArea,
+	ClientDto client
+) {
+}
