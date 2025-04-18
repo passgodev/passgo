b1d7d0bb49216be0135867c51dc93ec217ea4983
Pajtus <piotrus.umanski@gmail.com>
2025-04-18 15:57:58 +0200

    feat(transaction): implemented service for retrieval information about transactions

---
+++ src/main/java/pl/uj/passgo/services/transaction/TransactionService.java
@@ -0,0 +1,30 @@
+package pl.uj.passgo.services.transaction;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import pl.uj.passgo.mappers.transaction.TransactionMapper;
+import pl.uj.passgo.models.DTOs.transaction.TransactionDto;
+import pl.uj.passgo.repos.transaction.TransactionRepository;
+
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class TransactionService {
+	private final TransactionRepository transactionRepository;
+	private final TransactionMapper transactionMapper;
+
+	public Page<TransactionDto> getTransactions(Pageable pageable) {
+		return transactionRepository.findAll(pageable).map(transactionMapper::toDto);
+	}
+
+	public TransactionDto getTransaction(Long id) {
+		return transactionRepository.findById(id)
+			.map(transactionMapper::toDto)
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+}
