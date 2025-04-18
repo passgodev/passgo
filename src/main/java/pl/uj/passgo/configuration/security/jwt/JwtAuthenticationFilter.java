fc8eee857e387f212b94afd2de5f6fdc23b929c2
Pajtus <piotrus.umanski@gmail.com>
2025-04-18 12:04:08 +0200

    fix(jwtFilter): fixed wrong response codes

--- src/main/java/pl/uj/passgo/configuration/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/pl/uj/passgo/configuration/security/jwt/JwtAuthenticationFilter.java
@@ -54,5 +54,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 				SecurityContextHolder.getContext().setAuthentication(authToken);
 			}
 		}
+
+		filterChain.doFilter(request, response);
 	}
 }
