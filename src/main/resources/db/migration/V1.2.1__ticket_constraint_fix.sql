148bf2464cc6d76faa1388cf4f5a84a8e54b5f4f
Pajtus <piotrus.umanski@gmail.com>
2025-04-19 16:48:28 +0200

    refactor(flyway): moved changes to different entities to separate files

--- src/main/resources/db/migration/V1.2.1__ticket_constraint_fix.sql
+++ src/main/resources/db/migration/V1.2.1__ticket_constraint_fix.sql
@@ -1,4 +1,2 @@
 ALTER TABLE "ticket" DROP CONSTRAINT if exists "ticket_seat_id_key";
 ALTER TABLE "ticket" DROP CONSTRAINT if exists "ticket_client_id_key";
-
-ALTER TABLE "transaction" DROP COLUMN if exists "event_id";
