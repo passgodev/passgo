b28300dc17fb2af80b5a6732b941fcaa4a3c4cce
Pajtus <piotrus.umanski@gmail.com>
2025-04-19 16:46:34 +0200

    fix(ticket): changed ticket's entity constraint of seat_id and client_id foreign keys to drop unique constraint

--- src/main/resources/db/migration/V1.2.1__ticket_constraint_fix.sql
+++ src/main/resources/db/migration/V1.2.1__ticket_constraint_fix.sql
@@ -1,1 +1,4 @@
+ALTER TABLE "ticket" DROP CONSTRAINT if exists "ticket_seat_id_key";
+ALTER TABLE "ticket" DROP CONSTRAINT if exists "ticket_client_id_key";
+
 ALTER TABLE "transaction" DROP COLUMN if exists "event_id";
