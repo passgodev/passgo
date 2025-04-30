ebf5ad4a90f7762284915b61423f6c909c689369
Pajtus <piotrus.umanski@gmail.com>
2025-04-30 11:42:50 +0200

    feat(activationEndpoint): added activation endpoint of registered organizer

--- src/main/java/pl/uj/passgo/controllers/member/MemberController.java
+++ src/main/java/pl/uj/passgo/controllers/member/MemberController.java
@@ -6,12 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import pl.uj.passgo.models.member.MemberType;
 import pl.uj.passgo.models.responses.member.MemberResponse;
+import pl.uj.passgo.models.responses.member.OrganizerMemberResponse;
 import pl.uj.passgo.services.member.MemberService;
 
 
@@ -22,9 +21,16 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@GetMapping
+	@PreAuthorize("hasRole('ADMINISTRATOR')")
 	public ResponseEntity<Page<MemberResponse>> getMembers(@RequestParam(value = "type", required = true) MemberType type, @PageableDefault Pageable pageable) {
 		var membersResponse = memberService.getMembersByType(type, pageable);
 		return ResponseEntity.ok(membersResponse);
 	}
 
+	@PatchMapping("/{organizer-id}/activation")
+	@PreAuthorize("hasRole('ADMINISTRATOR')")
+	public ResponseEntity<OrganizerMemberResponse> activateOrganizer(@PathVariable("organizer-id") Long organizerId) {
+		var approvedOrganizerResponse = memberService.activateOrganizer(organizerId);
+		return ResponseEntity.ok(approvedOrganizerResponse);
+	}
 }
--- src/main/java/pl/uj/passgo/services/member/MemberService.java
+++ src/main/java/pl/uj/passgo/services/member/MemberService.java
@@ -1,19 +1,24 @@
 package pl.uj.passgo.services.member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import pl.uj.passgo.mappers.member.MemberResponseMapper;
 import pl.uj.passgo.models.member.MemberType;
+import pl.uj.passgo.models.member.Organizer;
 import pl.uj.passgo.models.responses.member.MemberResponse;
+import pl.uj.passgo.models.responses.member.OrganizerMemberResponse;
 import pl.uj.passgo.repos.member.ClientRepository;
 import pl.uj.passgo.repos.member.OrganizerRepository;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class MemberService {
@@ -35,5 +40,22 @@ public class MemberService {
 
 	private Page<MemberResponse> getOrganizers(Pageable pageable) {
 		return organizerRepository.findAll(pageable).map(memberResponseMapper::toOrganizerMemberResponse);
+	}
+
+	@Transactional
+	public OrganizerMemberResponse activateOrganizer(Long organizerId) {
+		return organizerRepository.findById(organizerId)
+								  .map(MemberService::activateOrganizer)
+								  .map(memberResponseMapper::toOrganizerMemberResponse)
+								  .orElseThrow(() -> {
+									  var message = String.format("Organizer with id: %d not found", organizerId);
+									  log.error(message);
+									  return new ResponseStatusException(HttpStatus.NOT_FOUND, message);
+								  });
+	}
+
+	private static Organizer activateOrganizer(Organizer organizer) {
+		organizer.setActive(true);
+		return organizer;
 	}
 }
