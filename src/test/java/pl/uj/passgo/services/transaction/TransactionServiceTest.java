45d39e07c9d0850449dd1b76e78ebcd9ab9eaa99
Pajtus <piotrus.umanski@gmail.com>
2025-05-23 20:08:18 +0200

    fix(transactionServiceTest): fixed instantiation of tested service

--- src/test/java/pl/uj/passgo/services/transaction/TransactionServiceTest.java
+++ src/test/java/pl/uj/passgo/services/transaction/TransactionServiceTest.java
@@ -1,10 +1,10 @@
 package pl.uj.passgo.services.transaction;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageImpl;
@@ -14,12 +14,12 @@ import pl.uj.passgo.mappers.client.ClientMapper;
 import pl.uj.passgo.mappers.event.EventMapper;
 import pl.uj.passgo.mappers.ticket.TicketMapper;
 import pl.uj.passgo.mappers.transaction.TransactionMapper;
-import pl.uj.passgo.models.DTOs.transaction.TransactionDto;
+import pl.uj.passgo.models.member.Client;
+import pl.uj.passgo.models.member.MemberCredential;
 import pl.uj.passgo.models.transaction.Transaction;
 import pl.uj.passgo.repos.transaction.TransactionRepository;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,11 +32,15 @@ public class TransactionServiceTest {
 	@Mock
 	private TransactionRepository transactionRepository;
 
-	@Mock
 	private TransactionMapper transactionMapper;
 
-	@InjectMocks
 	private TransactionService transactionService;
+
+	@BeforeEach
+	public void setup() {
+		this.transactionMapper = getTransactionMapper();
+		transactionService = new TransactionService(transactionRepository, transactionMapper);
+	}
 
 	@Test
 	public void testGetTransactions() {
@@ -68,9 +72,10 @@ public class TransactionServiceTest {
 	public void testGetTransactionById_found() {
 		// arrange
 		var transactionId = 1L;
+		var mockTransaction = getValidTransaction();
+		mockTransaction.setId(transactionId);
 		when(transactionRepository.findById(transactionId))
-			.thenReturn(Optional.of(getValidTransaction()));
-		when(transactionMapper.toDto(any(Transaction.class))).thenReturn(getTransactionMapper());
+			.thenReturn(Optional.of(mockTransaction));
 
 		// act
 		var returnedTransacstion = transactionService.getTransaction(transactionId);
@@ -80,10 +85,19 @@ public class TransactionServiceTest {
 	}
 
 	private static Transaction getValidTransaction() {
-		return new Transaction();
+		var client = new Client();
+		client.setMemberCredential(new MemberCredential());
+		var transaction = new Transaction();
+		transaction.setClient(client);
+		transaction.setTransactionComponents(Collections.emptyList());
+
+		return transaction;
 	}
 
-	private TransactionDto getTransactionMapper() {
-		return new TransactionDto(1l, BigDecimal.ZERO, LocalDateTime.now(), null, null);
+	private TransactionMapper getTransactionMapper() {
+		var clientMapper = new ClientMapper();
+		var eventMapper = new EventMapper();
+		var ticketMapper = new TicketMapper(eventMapper, clientMapper);
+		return new TransactionMapper(clientMapper, ticketMapper);
 	}
 }
