bf07bb782c1763d30c636ce1d520318fc88442e1
Pajtus <piotrus.umanski@gmail.com>
2025-05-23 14:41:44 +0200

    feat(memberServiceTest): added getMembersByType test and activateOrganiser test

--- src/test/java/pl/uj/passgo/services/member/MemberServiceTest.java
+++ src/test/java/pl/uj/passgo/services/member/MemberServiceTest.java
@@ -9,11 +9,18 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.server.ResponseStatusException;
+import pl.uj.passgo.mappers.client.ClientMapper;
 import pl.uj.passgo.mappers.member.MemberResponseMapper;
+import pl.uj.passgo.mappers.organizer.OrganizerMapper;
+import pl.uj.passgo.models.DTOs.member.OrganizerDto;
 import pl.uj.passgo.models.member.MemberCredential;
 import pl.uj.passgo.models.member.MemberType;
+import pl.uj.passgo.models.member.Organizer;
+import pl.uj.passgo.models.responses.member.OrganizerMemberResponse;
 import pl.uj.passgo.repos.member.ClientRepository;
 import pl.uj.passgo.repos.member.OrganizerRepository;
 import pl.uj.passgo.services.LoggedInMemberContextService;
@@ -21,8 +28,7 @@ import pl.uj.passgo.services.LoggedInMemberContextService;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,6 +41,9 @@ public class MemberServiceTest {
 	private MemberResponseMapper memberResponseMapper;
 	@Mock
 	private LoggedInMemberContextService loggedInMemberContextService;
+
+	@Spy
+	private OrganizerMapper organizerMapper;
 
 	@InjectMocks
 	private MemberService memberService;
@@ -115,5 +124,52 @@ public class MemberServiceTest {
 			memberType,
 			true
 		);
+	}
+
+	@Test
+	public void testGetMembersById_cannotGetAdmin() {
+		// arrange
+		var memberType = MemberType.ADMINISTRATOR;
+
+		// act
+		Executable shouldFail = () -> memberService.getMembersByType(memberType, Pageable.unpaged());
+
+		// assert
+		Assertions.assertThrows(ResponseStatusException.class, shouldFail);
+	}
+
+	@Test
+	public void testActivateOrganizer_organizerNotExist() {
+		// arrange
+		Long organizerId = 1L;
+		when(organizerRepository.findById(organizerId)).thenReturn(Optional.empty());
+
+		// act
+		Executable shouldFail = () -> memberService.activateOrganizer(organizerId);
+
+		// assert
+		Assertions.assertThrows(ResponseStatusException.class, shouldFail);
+	}
+
+	@Test
+	public void testActivateOrganizer_ativated() {
+		// arrange
+		// create organizer object
+		var organizerCredential = getValidMemberCredential(MemberType.ORGANIZER);
+		var organizerId = organizerCredential.getId();
+		var organizer = new Organizer();
+		organizer.setId(organizerId);
+		organizer.setMemberCredential(organizerCredential);
+		when(organizerRepository.findById(eq(organizerId))).thenReturn(Optional.of(organizer));
+		// mock mapper to obtain mapped organizer
+		var mappedOrganizer = organizerMapper.toOrganizerDto(organizer);
+		when(memberResponseMapper.toOrganizerMemberResponse(any(Organizer.class)))
+			.thenReturn(new OrganizerMemberResponse(mappedOrganizer));
+
+		// act
+		memberService.activateOrganizer(organizerId);
+
+		// assert
+		Assertions.assertTrue(organizerCredential.isActive());
 	}
 }
